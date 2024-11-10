c4f2cc4b8b305a7ee1847670b6320bd59989aa94
kind012 <tranthienduc1001@gmal.com>
2024-11-10 15:53:29 +0700

    config vercel and vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: "./",
+  base: "/",
   server: {
     proxy: {
       "/api": {
